fbb05d0aaa16237695cee45efeaf21220dd47757
Ken-muigai <kenmuigai980@gmail.com>
2020-11-22 00:40:49 +0300

    adding backend

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.json);
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message:"Arise Mern Developers"
+    })
+    res.status(200)
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Application is running on ${port}`);
+});
+
+
